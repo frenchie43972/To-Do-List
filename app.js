510932e9219c55962de0ab4864d92aaed1200b12
Kris French <frenchie43972@gmail.com>
2022-02-24 10:14:59 +0900

    Finished but could use some tidying up with functionality

--- app.js
+++ app.js
@@ -1,43 +1,134 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const req = require("express/lib/request");
-const date = require(__dirname + "/date.js");
-const app = express();
+const mongoose = require("mongoose");
+const _ = require("lodash");
+// const req = require("express/lib/request");
 
-let items = [];
-let workItems = [];
+const app = express();
 
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/todolistDB");
+
+
+const itemsSchema = {
+    name: String
+};
+const Item = mongoose.model("Item", itemsSchema);
+const itemOne = new Item({
+    name: "Wake Up"
+});
+const itemTwo = new Item({
+    name: "Make Bed"
+});
+const itemThree = new Item({
+    name: "Drink Water"
+});
+const defaultItems = [itemOne, itemTwo, itemThree];
+
+
+const listSchema = {
+    name: String,
+    items: [itemsSchema]
+};
+const List = mongoose.model("List", listSchema);
+
+
 app.get("/", (req, res) => {
-    let day = date.getDate();
-    res.render("list", {listTitle: day, newListItem: items});
+    // Adds default items to the list if the list is empty 
+    Item.find({}, (err, foundItems) => {
+        if (foundItems.length === 0) {
+            Item.insertMany(defaultItems, (err) => {
+                if (err) {
+                    console.log(err);
+                }
+                else {
+                    console.log("Good to go");
+                }
+            });
+            res.redirect("/");
+        }
+        else {
+            res.render("list", {listTitle: "Today", newListItem: foundItems});
+        }
+    });    
 });
 
-app.post("/", (req, res) => {
-    let item = req.body.newItem;
+// Creates a new list and a different page where the list can be accessed
+app.get("/:customListName", (req, res) => {
+    const customListName = _.capitalize(req.params.customListName);
 
-    if (req.body.list === "Work") {
-        workItems.push(item);
-        res.redirect("/work");
+    List.findOne({name: customListName}, (err, foundList) => {
+        if(!err) {
+            if(!foundList) {
+                const list = new List({
+                    name: customListName,
+                    items: defaultItems
+                });
+
+                list.save();
+                res.redirect("/" + customListName);
+            }
+            else {
+                res.render("list", {listTitle: foundList.name, newListItem: foundList.items});
+            }
+        }       
+    });
+});
+
+// This ensures the items add to the specific list stays with that list only
+app.post("/", (req, res) => {
+    const itemName = req.body.newItem;
+    const listName = req.body.list;
+
+    const item = new Item({
+        name: itemName
+    });
+
+    if (listName === "Today") {
+        item.save();
+        res.redirect("/");
     }
     else {
-    items.push(item);
-    res.redirect("/");
+        List.findOne({name: listName}, (err, foundList) => {
+            foundList.items.push(item);
+            foundList.save();
+            res.redirect("/" + listName);
+        });
     }
-
 });
 
-app.get("/work", (req, res) => {
-    res.render("list", {listTitle: "Work List", newListItem: workItems});
+// This will remove and item that is checked off
+app.post("/delete", (req, res) => {
+    const checkedItem = req.body.checkbox;
+    const listName = req.body.listName;
+
+    if (listName === "Today") {
+       Item.findByIdAndDelete(checkedItem, (err) => {
+        if (!err) {
+            console.log("Item deleted");
+        }
+        res.redirect("/");
+    }); 
+    }
+    else {
+        List.findOneAndUpdate({name: listName}, 
+            {$pull: {items: {_id: checkedItem}}}, (err, foundList) => {
+                res.redirect("/" + listName);
+        })
+    }
 });
 
-app.listen(3000, () => {
-    console.log("Server started on port 3000");
+app.get("/about", function(req, res){
+    res.render("about");
+  });
+
+app.listen(27017, () => {
+    console.log("Server started on port 27017");
 });
 
 

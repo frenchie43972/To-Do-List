a7f216d6a54ea992f13ca09039dd321babf6940c
Kris French <frenchie43972@gmail.com>
2022-03-01 12:32:54 +0900

    Removed db password. Still nee dto get thi sworking with Heroku

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://admin-kris:kris2551@cluster0.qrqx1.mongodb.net/todolistDB");
+// Re-input password
+mongoose.connect("mongodb+srv://admin-kris:********@cluster0.qrqx1.mongodb.net/todolistDB");
 
 
 const itemsSchema = {
